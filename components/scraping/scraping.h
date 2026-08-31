798b991ff465fd56af868d0c409beb60ce9a073a
Pavlo <tepasha.90@gmail.com>
2026-08-31 15:32:51 +0300

    refactor

--- components/scraping/scraping.h
+++ components/scraping/scraping.h
@@ -3,8 +3,8 @@
 #include "esp_event.h"
 #include "esp_http_client.h"
 
-static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
-static bool wifi_connect_sta();
-static esp_err_t http_event_handler(esp_http_client_event_t *evt);
-static int api_fetch_bearer_auth(const char *url, const char *token, char *outBuf, size_t outBufSize);
+void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+bool wifi_connect_sta();
+esp_err_t http_event_handler(esp_http_client_event_t *evt);
+int api_fetch_bearer_auth(const char *url, const char *token, char *outBuf, size_t outBufSize);
 void get_api(char apiurl[], char token[]);
